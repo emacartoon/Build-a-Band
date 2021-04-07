1a44f4cdc2c4349c233a0e23e6319389609c64a4
Daniel Lucas <danomiterock@yahoo.com>
2021-04-07 14:26:02 -0700

    update seed.js file

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,11 +1,29 @@
-const sequelize = require('../config/connection');
-const { User, Project } = require('../models');
+//sequelize and models
+const sequelize = require('../config/config');
+const { Band, Category, Post, Tag, User, UserBand } = require('../models');
 
-const userData = require('./userData.json');
-const projectData = require('./projectData.json');
+const bandData = require('./bandSeeds.json');
+const catData = require('./catSeeds.json');
+const postData = require('./postSeeds.json');
+const tagData = require('./tagSeeds.json');
+const userBandData = require('./userBandSeeds.json');
+const userData = require('./userSeeds.json');
 
+//figure this out, Dan
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
+
+  const categories = await User.bulkCreate(catData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const user of catData) {
+    await Category.create({
+      ...category,
+      category_id: category[Math.floor(Math.random() * category.length)].id,
+    });
+  }
 
   const users = await User.bulkCreate(userData, {
     individualHooks: true,
