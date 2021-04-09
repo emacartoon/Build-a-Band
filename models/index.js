33440670c84fe131391a7aaba42dbda3e2dca25d
EmaCartoon <emacartoon@gmail.com>
2021-04-09 09:51:48 -0700

    Trying to hunt down seeding error

--- models/index.js
+++ models/index.js
@@ -1,9 +1,9 @@
 //require all models
 const User = require('./User');
 const Post = require('./Post');
-const Tag = require('./Tags');
+// const Tag = require('./Tags');
 const Band = require('./Band');
-const Category = require('./Category');
+// const Category = require('./Category');
 
 //Define User model relations
 User.hasMany(Post, {
@@ -15,21 +15,21 @@ Post.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-User.hasMany(Tag, {
-  foreignKey: 'user_id',
-});
+// User.hasMany(Tag, {
+//   foreignKey: 'user_id',
+// });
   
-Tag.belongsTo(User, {
-  foreignKey: 'user_id'
-});
+// Tag.belongsTo(User, {
+//   foreignKey: 'user_id'
+// });
 
-User.hasMany(Category, {
-  foreignKey: 'user_id',
-});
+// User.hasMany(Category, {
+//   foreignKey: 'user_id',
+// });
     
-Category.belongsTo(User, {
-  foreignKey: 'user_id'
-});
+// Category.belongsTo(User, {
+//   foreignKey: 'user_id'
+// });
 
 //Create Post model relations
 Post.hasOne(User, {
@@ -48,48 +48,37 @@ Post.hasOne(User, {
     foreignKey: 'post_id'
   });
   
-  Post.hasMany(Category, {
-    foreignKey: 'post_id',
-    onDelete: 'CASCADE',
-  });
+  // Post.hasMany(Category, {
+  //   foreignKey: 'post_id',
+  //   onDelete: 'CASCADE',
+  // });
         
-  Category.belongsTo(Post, {
-    foreignKey: 'post_id'
-  });
+  // Category.belongsTo(Post, {
+  //   foreignKey: 'post_id'
+  // });
   
-  Post.hasMany(Tag, {
-    foreignKey: 'post_id',
-  });
+  // Post.hasMany(Tag, {
+  //   foreignKey: 'post_id',
+  // });
       
-  Tag.belongsTo(Post, {
-    foreignKey: 'post_id'
-  });
+  // Tag.belongsTo(Post, {
+  //   foreignKey: 'post_id'
+  // });
 
 //Create Tag model relations
-Tag.hasMany(User, {
-    foreignKey: 'tag_id',
-  });
-    
-  User.belongsTo(Tag, {
-    foreignKey: 'tag_id'
-  });
+// Tag.hasMany(User, {
+//     foreignKey: 'tag_id',
+//   });
   
-  Tag.hasMany(Post, {
-    foreignKey: 'tag_id',
-    onDelete: 'CASCADE',
-  });
+  // Tag.hasMany(Post, {
+  //   foreignKey: 'tag_id',
+  //   onDelete: 'CASCADE',
+  // });
         
-  Post.belongsTo(Tag, {
-    foreignKey: 'tag_id'
-  });
-  
-  Tag.hasMany(Category, {
-    foreignKey: 'tag_id',
-  });
-      
-  Category.belongsTo(Tag, {
-    foreignKey: 'tag_id'
-  });
+  // Tag.hasMany(Category, {
+  //   foreignKey: 'tag_id',
+  // });
+
   
 //Create Band model relations
 Band.hasMany(Post, {
@@ -101,47 +90,36 @@ Post.belongsTo(Band, {
   foreignKey: 'band_id'
 });
 
-Band.hasMany(Tag, {
-  foreignKey: 'band_id',
-});
-    
-Tag.belongsTo(Band, {
-  foreignKey: 'band_id'
-});
+// Band.hasMany(Tag, {
+//   foreignKey: 'tag_id',
+// });
 
-Band.hasMany(Category, {
-  foreignKey: 'band_id',
-});
-      
-Category.belongsTo(Band, {
-  foreignKey: 'band_id'
-});
+
+// Band.hasMany(Category, {
+//   foreignKey: 'band_id',
+// });
 
 //Create Category model relations
-Category.hasMany(User, {
-    foreignKey: 'category_id',
-  });
+// Category.hasMany(User, {
+//     foreignKey: 'category_id',
+//   });
     
-  User.belongsTo(Category, {
-    foreignKey: 'category_id'
-  });
-  
-  Category.hasMany(Post, {
-    foreignKey: 'category_id',
-    onDelete: 'CASCADE',
-  });
+  // Category.hasMany(Post, {
+  //   foreignKey: 'category_id',
+  //   onDelete: 'CASCADE',
+  // });
         
-  Post.belongsTo(Category, {
-    foreignKey: 'category_id'
-  });
+  // Post.belongsTo(Category, {
+  //   foreignKey: 'category_id'
+  // });
   
-  Category.hasMany(Tag, {
-    foreignKey: 'category_id',
-  });
+  // Category.hasMany(Tag, {
+  //   foreignKey: 'category_id',
+  // });
       
-  Tag.belongsTo(Category, {
-    foreignKey: 'category_id'
-  });
+  // Tag.belongsTo(Category, {
+  //   foreignKey: 'category_id'
+  // });
   
 //Export all models
 module.exports = { User, Post, Band};
