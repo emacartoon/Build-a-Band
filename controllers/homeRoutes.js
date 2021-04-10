986d78d8ca5e662e5653e0abc36e126f0050fe39
Daniel Lucas <danomiterock@yahoo.com>
2021-04-10 10:57:37 -0700

    change login-signup route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -70,14 +70,14 @@ router.get('/post', withAuth, async (req, res) => {
   }
 });
 
-router.get('/login', (req, res) => {
+router.get('/login-signup', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
     res.redirect('/dashboard');
     return;
   }
 
-  res.render('login');
+  res.render('login-signup');
 });
 
 module.exports = router;
